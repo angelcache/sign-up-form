18eaaac0a93fd2932a42deaa02ca621d17fb321f
Angela <angdkay@gmail.com>
2025-07-03 00:56:58 -0600

    Add visiblity toggle between login and sign up form

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-const loginButton = document.querySelector(".log-in-text");
-console.log(loginButton);
+/**
+ * Toggles visibility between the login and sign-up forms
+ * when the corresponding text links are clicked.
+ */
+
+
+const loginText = document.querySelector(".log-in-text");
 const loginForm = document.querySelector("#log-in-form");
+const signupText = document.querySelector(".sign-up-text");
 const signupForm = document.querySelector("#sign-up-form");
 
-loginButton.addEventListener('click', () => {
-  loginForm.style.display = "block";
+loginText.addEventListener('click', () => {
+  loginForm.style.display = "flex";
   signupForm.style.display = "none";
+});
+
+signupText.addEventListener('click', () => {
+  loginForm.style.display = "none";
+  signupForm.style.display = "flex";
 });
 
